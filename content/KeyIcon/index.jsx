1100c180eb808afed0b1f220d7b96fa27d2de3eb
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-10-15 19:13:39 +0530

    feat: Added passme logo icon

--- content/KeyIcon/index.jsx
+++ content/KeyIcon/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Icon from './Icon'
 
+/**
+ * This component renders passme logo icon inside every input [type="password"]
+ * element that can be used to fill a specified password or generate a new
+ * password.
+ */
 function KeyIcon({ element }) {
-  return <span>Key</span>
+  return <Icon />
 }
 
 export default KeyIcon
